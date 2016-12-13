15b4e44efa8360ac2b29c4327ad11681b2c126b2
proxza <proxza@gmail.com>
2016-12-13 21:58:24 +0200

    PHWork2:3 | Fixed index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
+<head>
+    <title>PHWork2:Работа с массивами</title>
+    <style>
+        table {
+            border: 1px solid;
+            margin: 0 auto;
+        }
+    </style>
+</head>
 
 <table>
     <?php
@@ -11,23 +20,42 @@
 
     // Вывод и транспонирование первой матрицы
     foreach ($arr1 as $key){
-        echo "<tr>";
         foreach ($key as $test => $value) {
             $a = array_shift($arr1[$test]);
-            array_push($arr2, $a);
-            echo "<td>" . $value . "</td>";
+            $arr2[] = $a; // Заполняем 2й пустой массив (транспорируем)
+            $arr3[] = $value; // Сохраняем в новую переменную наш исходный массив так как из исходного все ключи будут "изьяты" array_shift'ом
         }
-        echo "</tr>";
     }
 
-    // Вывод второй транспонированной матрицы
-    foreach ($arr2 as $str){
-        echo "<tr>";
-        echo "<td>" .$str. "</td>";
-        echo "</tr>";
+    // Слаживаем массивы
+    for ($i = 1;$i <= 9;$i++){
+        array_push($arr3, $arr2[$i]);
     }
 
-    print_r($arr2);
+    // Изобретаем Велосипед!
+
+    $b = 0; // Инициализируем пустую переменную
+
+    // Пуляем наш первый цикл для 1го толбца
+    echo "<tr>";
+    while ($b <= 8)
+    {
+        echo "<td>" . $arr3[$b] . "</td>";
+        $b++;
+    }
+    echo "</tr>";
+
+    $c = 9;
+    // Пуляем наш второй цикл для 2го столбца
+    echo "<tr>";
+    while ($c <= 17)
+    {
+        echo "<td>" . $arr3[$c] . "</td>";
+        $c++;
+    }
+    echo "</tr>";
+
+    //var_export($arr3);
 
     ?>
 </table>
